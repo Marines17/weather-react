bf6d9e76c8af8c78b80b1ab1ff1a1f816f9bbcbb
Marines17 <simon.marine33@gmail.com>
2021-06-09 11:25:49 +0100

    Updated return function

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -29,67 +29,133 @@ export default function SearchEngine() {
     setCity(event.target.value);
   }
 
-  return (
-    <div>
-      <h1>
-        <div className="row">
-          <div className="col-6">
-            <strong id="city">{city}</strong>
-            <br />
-            <span className="time">12:48</span>
-            <br />
-            <span className="today">Thursday</span>
-            <br />
-          </div>
-          <div className="col-6 right-text">
-            <span className="bdxicon">
-              <img
-                src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-                alt="Weather icon"
-              />
-            </span>
-            <span className="bdxtemp">
-              <span id="temperature">{temp}</span>
-              <span className="celcius" id="convertCelcius">
-                <a href="/" id="celcius-link" className="active">
-                  °C
-                </a>
+  if (temp) {
+    return (
+      <div>
+        <h1>
+          <div className="row">
+            <div className="col-6">
+              <strong id="city">{city}</strong>
+              <br />
+              <span className="time">12:48</span>
+              <br />
+              <span className="today">Thursday</span>
+              <br />
+            </div>
+            <div className="col-6 right-text">
+              <span className="bdxicon">
+                <img
+                  src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                  alt="Weather icon"
+                />
               </span>
-              <span className="fahrenheit" id="convertFahrenheit">
-                <a href="/" id="fahrenheit-link" className="active">
-                  /°F
-                </a>
-              </span>{" "}
-            </span>
-            <br />
-            <ul>
-              <li className="feelsLike" id="feelsLike">
-                Feels like: {feelslike}℃
-              </li>
+              <span className="bdxtemp">
+                <span id="temperature">{temp}</span>
+                <span className="celcius" id="convertCelcius">
+                  <a href="/" id="celcius-link" className="active">
+                    °C
+                  </a>
+                </span>
+                <span className="fahrenheit" id="convertFahrenheit">
+                  <a href="/" id="fahrenheit-link" className="active">
+                    /°F
+                  </a>
+                </span>{" "}
+              </span>
               <br />
-              <li className="humidity" id="humidity">
-                Humidity: {humidity}%
-              </li>
-              <br />
-              <li className="wind" id="wind">
-                Wind: {wind}km/h
-              </li>
-            </ul>
+              <ul>
+                <li className="feelsLike" id="feelsLike">
+                  Feels like: {feelslike}℃
+                </li>
+                <br />
+                <li className="humidity" id="humidity">
+                  Humidity: {humidity}%
+                </li>
+                <br />
+                <li className="wind" id="wind">
+                  Wind: {wind}km/h
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </h1>
-      <form id="search-form" onSubmit={HandleSubmit}>
-        <input
-          type="search"
-          onChange={updateCity}
-          placeholder="Search for a city"
-          autocomplete="off"
-          id="search-text-input"
-          className="searchForm"
-        />
-        <input className="search-form" type="submit" value="☀️" />
-        <input className="submitPin" type="submit" value="📍" />
-      </form>
-    </div>
-  );
+        </h1>
+        <form id="search-form" onSubmit={HandleSubmit}>
+          <input
+            type="search"
+            onChange={updateCity}
+            placeholder="Search for a city"
+            autoComplete="off"
+            id="search-text-input"
+            className="searchForm"
+          />
+          <input className="search-form" type="submit" value="☀️" />
+          <input className="submitPin" type="submit" value="📍" />
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          <div className="row">
+            <div className="col-6">
+              <strong id="city">Bordeaux</strong>
+              <br />
+              <span className="time">12:48</span>
+              <br />
+              <span className="today">Thursday</span>
+              <br />
+            </div>
+            <div className="col-6 right-text">
+              <span className="bdxicon">
+                <img
+                  src={`https://openweathermap.org/img/wn/01d@2x.png`}
+                  alt="Weather icon"
+                />
+              </span>
+              <span className="bdxtemp">
+                <span id="temperature">28</span>
+                <span className="celcius" id="convertCelcius">
+                  <a href="/" id="celcius-link" className="active">
+                    °C
+                  </a>
+                </span>
+                <span className="fahrenheit" id="convertFahrenheit">
+                  <a href="/" id="fahrenheit-link" className="active">
+                    /°F
+                  </a>
+                </span>{" "}
+              </span>
+              <br />
+              <ul>
+                <li className="feelsLike" id="feelsLike">
+                  Feels like: 31℃
+                </li>
+                <br />
+                <li className="humidity" id="humidity">
+                  Humidity: 15%
+                </li>
+                <br />
+                <li className="wind" id="wind">
+                  Wind: 2km/h
+                </li>
+              </ul>
+            </div>
+          </div>
+        </h1>
+        <form id="search-form" onSubmit={HandleSubmit}>
+          <input
+            type="search"
+            onChange={updateCity}
+            placeholder="Search for a city"
+            autoComplete="off"
+            id="search-text-input"
+            className="searchForm"
+          />
+          <input className="search-form" type="submit" value="☀️" />
+          <input className="submitPin" type="submit" value="📍" />
+        </form>
+      </div>
+    );
+  }
 }
