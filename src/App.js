c7e71663cd560552eb166832da0dff83544eaf7c
Marines17 <simon.marine33@gmail.com>
2021-06-09 11:18:03 +0100

    Created Search Engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Heading from "./Heading";
 import SearchEngine from "./SearchEngine";
 import "./styles.css";
 
@@ -7,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Heading />
         <SearchEngine />
       </div>
     </div>
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,27 +1,95 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
-  // eslint-disable-next-line
   let [city, setCity] = useState("Bordeaux");
+  let [temp, setTemp] = useState(null);
+  let [feelslike, setFeelslike] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function displayWeather(response) {
+    console.log(response);
+    setTemp(Math.round(response.data.main.temp));
+    setFeelslike(Math.round(response.data.main.feels_like));
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+    setIcon(response.data.weather[0].icon);
+  }
+
   function HandleSubmit(event) {
     event.preventDefault();
+    let apiKey = `66e48331c74ac5541e45dafb42039ad5`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayWeather);
   }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
 
   return (
-    <form id="search-form" onSubmit={HandleSubmit}>
-      <input
-        type="search"
-        onChange={updateCity}
-        placeholder="Search for a city"
-        autocomplete="off"
-        id="search-text-input"
-        className="searchForm"
-      />
-      <input className="search-form" type="submit" value="☀️" />
-      <input className="submitPin" type="submit" value="📍" />
-    </form>
+    <div>
+      <h1>
+        <div className="row">
+          <div className="col-6">
+            <strong id="city">{city}</strong>
+            <br />
+            <span className="time">12:48</span>
+            <br />
+            <span className="today">Thursday</span>
+            <br />
+          </div>
+          <div className="col-6 right-text">
+            <span className="bdxicon">
+              <img
+                src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                alt="Weather icon"
+              />
+            </span>
+            <span className="bdxtemp">
+              <span id="temperature">{temp}</span>
+              <span className="celcius" id="convertCelcius">
+                <a href="/" id="celcius-link" className="active">
+                  °C
+                </a>
+              </span>
+              <span className="fahrenheit" id="convertFahrenheit">
+                <a href="/" id="fahrenheit-link" className="active">
+                  /°F
+                </a>
+              </span>{" "}
+            </span>
+            <br />
+            <ul>
+              <li className="feelsLike" id="feelsLike">
+                Feels like: {feelslike}℃
+              </li>
+              <br />
+              <li className="humidity" id="humidity">
+                Humidity: {humidity}%
+              </li>
+              <br />
+              <li className="wind" id="wind">
+                Wind: {wind}km/h
+              </li>
+            </ul>
+          </div>
+        </div>
+      </h1>
+      <form id="search-form" onSubmit={HandleSubmit}>
+        <input
+          type="search"
+          onChange={updateCity}
+          placeholder="Search for a city"
+          autocomplete="off"
+          id="search-text-input"
+          className="searchForm"
+        />
+        <input className="search-form" type="submit" value="☀️" />
+        <input className="submitPin" type="submit" value="📍" />
+      </form>
+    </div>
   );
 }
